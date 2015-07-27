6ef917fb29cbdb3fa072dcb4f16235ba454c0838
chmln <chmln@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 23:49:07 +0000

    new font
    git-svn-id: https://plugins.svn.wordpress.org/material-admin-theme/trunk@1207903 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- material-admin-theme.php
+++ material-admin-theme.php
@@ -5,14 +5,14 @@ Plugin Name: Material Admin Theme
 Plugin URI:
 Description: Clean, eye-pleasing theme
 Author: Gregory Petrosyan
-Version: 1.0.1
+Version: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 function material_style() {
     wp_enqueue_style('material-theme', plugins_url('assets/material.css', __FILE__));
-     wp_enqueue_style('material-nunito', '//fonts.googleapis.com/css?family=Nunito:400,300,500,600,700');
+     wp_enqueue_style('material-nunito', '//fonts.googleapis.com/css?family=Raleway:400,300,500,600,700');
 }
 add_action('admin_enqueue_scripts', 'material_style');
 add_action('login_enqueue_scripts', 'material_style');
