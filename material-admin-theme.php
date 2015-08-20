45588b5b013a1c711b69dc7d1c2ae9dce6a17f29
chmln <chmln@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-20 23:20:23 +0000

    tag updates
    git-svn-id: https://plugins.svn.wordpress.org/material-admin-theme/tags/1.1@1226622 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- material-admin-theme.php
+++ material-admin-theme.php
@@ -5,7 +5,7 @@ Plugin Name: Material Admin Theme
 Plugin URI:
 Description: Clean, eye-pleasing theme
 Author: Gregory Petrosyan
-Version: 1.0.1
+Version: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
