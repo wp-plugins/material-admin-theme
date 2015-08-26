1d63636e017a00a208b08a6270c3c15e985dc23e
chmln <chmln@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-26 04:41:08 +0000

    upd author name
    git-svn-id: https://plugins.svn.wordpress.org/material-admin-theme/trunk@1230961 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- material-admin-theme.php
+++ material-admin-theme.php
@@ -4,7 +4,7 @@
 Plugin Name: Material Admin Theme
 Plugin URI:
 Description: Clean, eye-pleasing theme
-Author: Gregory Petrosyan
+Author: chmln
 Version: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -12,8 +12,10 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 function material_style() {
     wp_enqueue_style('material-theme', plugins_url('assets/material.css', __FILE__));
-     wp_enqueue_style('material-raleway', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700');
+    wp_enqueue_style('material-raleway', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700');
 }
+
 add_action('admin_enqueue_scripts', 'material_style');
 add_action('login_enqueue_scripts', 'material_style');
+
 ?>
